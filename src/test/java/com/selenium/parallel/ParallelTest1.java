1cfe8450f1e9042ea14769c2f40c1998f47f4734
Vidor Huang <vidor.huang@ehealth.com>
2017-10-12 15:26:15 +0800

    null

--- src/test/java/com/selenium/parallel/ParallelTest1.java
+++ src/test/java/com/selenium/parallel/ParallelTest1.java
@@ -1,11 +1,27 @@
 package com.selenium.parallel;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
  * Created by vidorh on 6/10/2017.
  */
 public class ParallelTest1 {
+    @BeforeSuite
+    public void testt(){
+        System.out.println("这是一个suite1");
+    }
+    @BeforeTest
+    public void testtest(){
+        System.out.println("这是一个 beforetest1");
+    }
+
+    @BeforeClass
+    public void testttt(){
+        System.out.println("这是一个 beforeClass");
+    }
     @Test
     public void test1() throws InterruptedException {
         System.out.println("test1");
--- src/test/java/com/selenium/parallel/ParallelTest2.java
+++ src/test/java/com/selenium/parallel/ParallelTest2.java
@@ -1,11 +1,26 @@
 package com.selenium.parallel;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
  * Created by vidorh on 6/10/2017.
  */
 public class ParallelTest2 {
+    @BeforeSuite
+    public void testt(){
+        System.out.println("这是一个suite2");
+    }
+    @BeforeTest
+    public void testtest(){
+        System.out.println("这是一个 beforetest2");
+    }
+    @BeforeClass
+    public void testttt(){
+        System.out.println("这是一个 beforeClass");
+    }
     @Test
     public void test5() throws InterruptedException {
         System.out.println("test5");
